39e8babd4e63b19cc2e18607848ccb7d0142325b
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2022-02-23 13:26:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,29 @@ function populatePage() {
     fetch("domain.json")
       .then((req) => req.json())
       .then((data) => {
-        const header = document.querySelector(".header");
-        header.innerHTML = "";
+      keys = Object.keys(data[0]);
+      var table = document.createElement("table");
+      var tr = table.insertRow(-1);
+      for (i=0;i<keys.length;i++) {
+        var th = document.createElement("th");
+        th.innerHTML = keys[i];
+        tr.appendChild(th);
+      }
         for (const item of data) {
-          keys = Object.keys(item);
-          console.log(keys);
-          for (i=0;i<keys.length;i++) {
-              console.log(keys[i]);
-              header.insertAdjacentHTML(
-                  "beforeend",
-                  `
-                    <th>${keys[i]}</th>
-                  `
-              )
-          }
-        //   header.insertAdjacentHTML(
-        //     "beforeend",
-        //     `
-        //     <td>${key}</td>`
-        //   );
+          console.log(item);
         }
-      });
-  }
+      for (var i = 0; i < data.length; i++) {
+    
+        tr = table.insertRow(-1);
 
-{/* <td></td> */}
+        for (var j = 0; j < keys.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = data[i][keys[j]];
+        }
+      }
+      var divContainer = document.getElementById("theTable");
+      divContainer.innerHTML = "";
+      divContainer.appendChild(table);  
+    }
+  )  
+}
